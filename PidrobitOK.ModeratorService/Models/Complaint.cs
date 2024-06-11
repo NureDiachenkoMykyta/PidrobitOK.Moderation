f92cc01f7f79f5407c4b3a52d485f751ba8c6793
NureDiachenkoMykyta <mykyta.diachenko1@nure.ua>
2024-06-11 14:10:00 +0300

    Add Complaint model

---
+++ PidrobitOK.ModeratorService/Models/Complaint.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PidrobitOK.ModeratorService.Models
+{
+    public class Complaint
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public Guid TargetId { get; set; }
+        public string Reason { get; set; }
+        public bool IsResolved { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid ModeratorId { get; set; }
+    }
+}
