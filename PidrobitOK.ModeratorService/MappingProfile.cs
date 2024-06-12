60be491b97e638148d65d7b074a9444ae8b1e368
NureDiachenkoMykyta <mykyta.diachenko1@nure.ua>
2024-06-12 13:07:00 +0300

    Add ComplaintDto and AutoMapper config

---
+++ PidrobitOK.ModeratorService/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using PidrobitOK.ModeratorService.Models.DTO;
+using PidrobitOK.ModeratorService.Models;
+
+namespace PidrobitOK.ModeratorService
+{
+    public class MapperProfile
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingCofig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Complaint, ComplaintDto>().ReverseMap();
+                config.CreateMap<JobModeration, JobModerationDto>().ReverseMap();
+                config.CreateMap<ModerationLog, ModerationLogDto>().ReverseMap();
+            });
+
+            return mappingCofig;
+        }
+    }
+}
---
+++ PidrobitOK.ModeratorService/Models/DTO/ComplaintDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PidrobitOK.ModeratorService.Models.DTO
+{
+    public class ComplaintDto
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public Guid TargetId { get; set; }
+        public string Reason { get; set; }
+        public bool IsResolved { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid ModeratorId { get; set; }
+    }
+}
